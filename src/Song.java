9bb5bce3927d5354f08e3794476459563e8ad472
Felix Chen <felixzc8@gmail.com>
2023-03-12 17:22:08 +0800

    finished

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,33 @@
-public class Song {
+import java.util.ArrayList;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+
+    ArrayList<String> listeners = new ArrayList<>();
+
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> listeners) {
+        int count = 0;
+        for (String listener : listeners) {
+            listener = listener.toLowerCase();
+            if (!this.listeners.contains(listener)) {
+                count++;
+                this.listeners.add(listener);
+            }
+        }
+        return count;
+    }
 }
